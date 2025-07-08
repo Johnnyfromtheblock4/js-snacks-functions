aaa7726db067a2b3987aa5ada7556191b9ec0569
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-08 18:03:02 +0200

    03-filtra_iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,11 +2,21 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+function filteredByFirstLetter(array, letter) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    let parola = array[i];
+    let parolaFirstLetter = parola[0];
+    if (parolaFirstLetter == letter) {
+      newArray.push(parola);
+    }
+  }
+  return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+let result = filteredByFirstLetter(names, `A`);
+console.log(result);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
