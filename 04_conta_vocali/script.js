c3cb3ae22509ce6fde8e8183047f303c68f04a9b
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-08 18:21:03 +0200

    [UPD] 04-conta_vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,20 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
-
+function countVocals(array) {
+  let count = 0;
+  const vocals = [`a`, `e`, `i`, `o`, `u`];
+  for (let i = 0; i < array.length; i++) {
+    if (vocals.includes(array[i])) {
+      count++;
+    }
+  }
+  return count;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(countVocals(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
