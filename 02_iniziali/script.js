320130f41b6e23e5b61dd754c0807936c09182b1
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-09 09:51:45 +0200

    [UPD] correzione-02

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,13 +4,16 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 function getFirstLetter(array) {
-  let newArray = [];
+  //definisco l'array delle iniziali
+  const initials = [];
+  //ciclo l'array
   for (let i = 0; i < array.length; i++) {
     const name = array[i];
     const firstLetter = name[0];
-    newArray.push(firstLetter);
+    //pusho le lettere nell'array vuoto
+    initials.push(firstLetter);
   }
-  return newArray;
+  return initials;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
@@ -18,3 +21,9 @@ let firstLetterBox = getFirstLetter(names);
 console.log(firstLetterBox);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+//ARROW FUNCTION
+
+const getIntilas = (array) => {
+  //passaggi come sopra
+};
